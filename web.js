64420e435fa9519b06adbe1062de90bc3b52c50f
Mandeep Shergill <mandeep@shergillgames.com>
2013-07-04 12:21:38 -0400

    Reading from index.html file

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var text = fs.readFileSync('./index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  response.write(text);
+  response.close();
 });
 
 var port = process.env.PORT || 5000;
